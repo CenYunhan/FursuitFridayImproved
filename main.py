820ca23151680c029a816b539875b8aee844e94a
熠云 <6cm@outlook.com>
2022-03-06 10:45:35 +0800

    添加了自由滚动模式

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 # import json #启用该行以调用cookie登录
 import os
 import re
-from typing import Tuple
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import sys
 
-if os.path.exists("URLs.txt"): #删除已经存在的urls
+if os.path.exists("URLs.txt"):  # 删除已经存在的urls
     os.remove("URLs.txt")
 
-'''
-    下面的是命令行模式（headless），取消注释可用
-'''
-# options = webdriver.ChromeOptions() 
-# options.add_argument("--headless")
-# options.add_argument("--disable-gpu")
+options = webdriver.ChromeOptions()
+arg = len(sys.argv)
 
-driver = webdriver.Chrome() #调用chrome，调用命令行在括号内加options=options
-URL = "https://t.bilibili.com/topic/8807683/" #fursuitfriday页面URL
+
+def urlDumper(beautifulsoup_response):
+    rule = re.compile(r'[(](.*?)[)]', re.S)  # 筛选规则，提取括号内URL
+    rawImageList = re.findall(rule, str(beautifulsoup_response))
+    converedURL = []
+    for raw in rawImageList:
+        process1 = raw.replace('"', "")
+        process2 = process1.find("@")
+        process1 = process1[:process2]
+        finalURL = process1.replace("//", "https://")
+        converedURL.append(finalURL)
+
+    return converedURL
+
+
+if arg != 1:
+    try:
+        command = int(sys.argv[1])
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+    except ValueError:
+        arg = 1
+        options = webdriver.ChromeOptions()
+        print("wrong input")
+
+driver = webdriver.Chrome(options=options)  # 调用chrome，调用命令行在括号内加options=options
+URL = "https://t.bilibili.com/topic/8807683/"  # fursuitfriday页面URL
 driver.get(URL)
 
 '''
@@ -26,40 +47,47 @@ driver.get(URL)
 #     cookieList = json.load(f)
 #     for cookie in cookieList:
 #         driver.add_cookie(cookie)
-#driver.refresh()
+# driver.refresh()
 
-driver.implicitly_wait(10)  #等待网页加载
+driver.implicitly_wait(10)  # 等待网页加载
 
-while True:
+while arg == 1:
     userInput = str(input("更多？"))
-    if userInput == "y" or "Y":
-        driver.execute_script('window.scrollBy(0,1000)')
+    if userInput == ("y" or "Y"):
+        driver.execute_script('window.scrollTo(0,window.document.body.scrollHeight)')
     else:
         break
 
-webpage = driver.page_source  #获取网页源代码
-
-driver.quit() #关闭浏览器 节省资源
+webpage = driver.page_source  # 获取网页源代码
 
 soup = BeautifulSoup(webpage, "lxml")
 # print(soup.prettify())
 
 # print(soup.prettify())
 
-lists = soup.find_all("div", {'class':'img-content'}) #寻找带img-content类的div（含图片框）
-rule = re.compile(r'[(](.*?)[)]', re.S) #筛选规则，提取括号内URL
+lists = soup.find_all("div", {'class': 'img-content'})  # 寻找带img-content类的div（含图片框）
+response = urlDumper(lists)  # 处理完毕的URL列表
 
+if arg != 1:
+    while len(response) < command:
+        driver.execute_script('window.scrollTo(0,window.document.body.scrollHeight)')
+        driver.implicitly_wait(10)
+        webpage = driver.page_source  # 获取网页源代码
 
-for raw in lists:
-    trans = str(raw)
-    pickZero = re.findall(rule,trans)[0]
-    process1 = pickZero.replace('"',"")
-    process2 = process1.find("@")
-    process1 = process1[:process2]
-    finalURL = process1.replace("//","https://")
-    
-    with open("URLs.txt","a+") as buffer:   #写入文件
-        buffer.write(finalURL+"\n")
+        soup = BeautifulSoup(webpage, "lxml")
+        # print(soup.prettify())
+
+        # print(soup.prettify())
+
+        lists = soup.find_all("div", {'class': 'img-content'})  # 寻找带img-content类的div（含图片框）
+        response = urlDumper(lists)  # 处理完毕的URL列表
+        response = response[:command]
+
+driver.quit()  # 关闭浏览器 节省资源
+
+with open("URLs.txt", "a+") as file:
+    for item in response:
+        file.write(item)
 
 print("Done")
 
