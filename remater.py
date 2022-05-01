9354f1a2db2f46ec19745e068a8df41da9b8061f
熠云 <6cm@outlook.com>
2022-05-01 14:04:08 +0800

    remaster version improved

--- remater.py
+++ remater.py
@@ -5,36 +5,48 @@ from urllib.request import urlretrieve
 
 
 def dumper(xhr, num):
-    global result
+    global results, image_count
 
     xhr_data = xhr["data"]
     offset = xhr_data['offset']
-    for item in xhr_data['cards']:
-        card = json.loads(item["card"])
+    for source in xhr_data['cards']:
+        if image_count >= num:
+            break
+        card = json.loads(source["card"])
         try:
+            images_temp = []
             for picture in card["item"]["pictures"]:
+                if image_count >= num:
+                    break
                 img_src = picture["img_src"]
-                result.append(img_src)
+                images_temp.append(img_src)
+                image_count += 1
+            user_name = card['user']['name']
+            profile = {
+                'name': user_name,
+                'images': images_temp
+            }
+            results.append(profile)
         except KeyError:
             pass
-    if len(result) > num:
-        result = result[:num]
-        pass
-    else:
+    if image_count < num:
         history_url = history_api_url + "topic_id=" + topic_id + "&offset_dynamic_id=" + offset
         history = requests.get(history_url).json()
         dumper(history, num)
 
-result = []
+
+results = []
 topic_id = "8807683"
 basic_api_url = "https://api.vc.bilibili.com/topic_svr/v1/topic_svr/topic_new?topic_id=" + topic_id
 history_api_url = "https://api.vc.bilibili.com/topic_svr/v1/topic_svr/topic_history?"
 web = requests.get(basic_api_url)
+image_count = 0
 xhr = web.json()
-dumper(xhr, 30)
-print(result)
-for i in result:
-    f = i[i.rfind("/") + 1:]
-    print(f)
-    urlretrieve(i, os.path.join(os.path.abspath("images"), f))
-print(len(result), result)
+dumper(xhr, 40)
+print(results)
+count = 0
+for item in results:
+    print("name", item["name"])
+    print("images", item["images"])
+    count += len(item["images"])
+print(count)
