3a4d7118c4e105ba5a9ee4398ec0ff986da5fa0d
熠云 <6cm@outlook.com>
2022-05-03 19:45:16 +0800

    加亿点点细节

--- browser.py
+++ browser.py
@@ -16,16 +16,16 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QHeaderView,
-    QLabel, QMainWindow, QMenu, QMenuBar,
-    QPushButton, QSizePolicy, QTreeWidget, QTreeWidgetItem,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QHBoxLayout,
+    QHeaderView, QLabel, QMainWindow, QMenu,
+    QMenuBar, QPushButton, QSizePolicy, QTreeWidget,
+    QTreeWidgetItem, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(407, 421)
+        MainWindow.resize(476, 556)
         self.action_download = QAction(MainWindow)
         self.action_download.setObjectName(u"action_download")
         self.action_about = QAction(MainWindow)
@@ -68,31 +68,75 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_4 = QLabel(self.centralwidget)
-        self.label_4.setObjectName(u"label_4")
-        sizePolicy1.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
-        self.label_4.setSizePolicy(sizePolicy1)
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.checkBox_1 = QCheckBox(self.centralwidget)
+        self.checkBox_1.setObjectName(u"checkBox_1")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.checkBox_1.sizePolicy().hasHeightForWidth())
+        self.checkBox_1.setSizePolicy(sizePolicy2)
+        self.checkBox_1.setMaximumSize(QSize(16777215, 10))
 
-        self.horizontalLayout_2.addWidget(self.label_4)
+        self.horizontalLayout_5.addWidget(self.checkBox_1)
 
-        self.label_5 = QLabel(self.centralwidget)
-        self.label_5.setObjectName(u"label_5")
-        sizePolicy1.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
-        self.label_5.setSizePolicy(sizePolicy1)
+        self.checkBox_2 = QCheckBox(self.centralwidget)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+        sizePolicy2.setHeightForWidth(self.checkBox_2.sizePolicy().hasHeightForWidth())
+        self.checkBox_2.setSizePolicy(sizePolicy2)
+        self.checkBox_2.setMaximumSize(QSize(16777215, 10))
 
-        self.horizontalLayout_2.addWidget(self.label_5)
+        self.horizontalLayout_5.addWidget(self.checkBox_2)
 
-        self.label_6 = QLabel(self.centralwidget)
-        self.label_6.setObjectName(u"label_6")
-        sizePolicy.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
-        self.label_6.setSizePolicy(sizePolicy)
+        self.checkBox_3 = QCheckBox(self.centralwidget)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+        sizePolicy2.setHeightForWidth(self.checkBox_3.sizePolicy().hasHeightForWidth())
+        self.checkBox_3.setSizePolicy(sizePolicy2)
+        self.checkBox_3.setMaximumSize(QSize(16777215, 10))
 
-        self.horizontalLayout_2.addWidget(self.label_6)
+        self.horizontalLayout_5.addWidget(self.checkBox_3)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout_5, 1, 0, 1, 1)
+
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.checkBox_7 = QCheckBox(self.centralwidget)
+        self.checkBox_7.setObjectName(u"checkBox_7")
+        sizePolicy2.setHeightForWidth(self.checkBox_7.sizePolicy().hasHeightForWidth())
+        self.checkBox_7.setSizePolicy(sizePolicy2)
+        self.checkBox_7.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_6.addWidget(self.checkBox_7)
+
+        self.checkBox_8 = QCheckBox(self.centralwidget)
+        self.checkBox_8.setObjectName(u"checkBox_8")
+        sizePolicy2.setHeightForWidth(self.checkBox_8.sizePolicy().hasHeightForWidth())
+        self.checkBox_8.setSizePolicy(sizePolicy2)
+        self.checkBox_8.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_6.addWidget(self.checkBox_8)
+
+        self.checkBox_9 = QCheckBox(self.centralwidget)
+        self.checkBox_9.setObjectName(u"checkBox_9")
+        sizePolicy2.setHeightForWidth(self.checkBox_9.sizePolicy().hasHeightForWidth())
+        self.checkBox_9.setSizePolicy(sizePolicy2)
+        self.checkBox_9.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_6.addWidget(self.checkBox_9)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_6, 6, 0, 1, 1)
+
+        self.treeWidget = QTreeWidget(self.centralwidget)
+        __qtreewidgetitem = QTreeWidgetItem(self.treeWidget)
+        __qtreewidgetitem.setFlags(Qt.ItemIsSelectable|Qt.ItemIsUserCheckable|Qt.ItemIsEnabled);
+        __qtreewidgetitem.setCheckState(0, Qt.Unchecked);
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.treeWidget.setMaximumSize(QSize(16777215, 100))
+
+        self.gridLayout.addWidget(self.treeWidget, 9, 0, 1, 1)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
@@ -118,7 +162,62 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3.addWidget(self.label_9)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_3, 2, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 4, 0, 1, 1)
+
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.checkBox_4 = QCheckBox(self.centralwidget)
+        self.checkBox_4.setObjectName(u"checkBox_4")
+        sizePolicy2.setHeightForWidth(self.checkBox_4.sizePolicy().hasHeightForWidth())
+        self.checkBox_4.setSizePolicy(sizePolicy2)
+        self.checkBox_4.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_7.addWidget(self.checkBox_4)
+
+        self.checkBox_5 = QCheckBox(self.centralwidget)
+        self.checkBox_5.setObjectName(u"checkBox_5")
+        sizePolicy2.setHeightForWidth(self.checkBox_5.sizePolicy().hasHeightForWidth())
+        self.checkBox_5.setSizePolicy(sizePolicy2)
+        self.checkBox_5.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_7.addWidget(self.checkBox_5)
+
+        self.checkBox_6 = QCheckBox(self.centralwidget)
+        self.checkBox_6.setObjectName(u"checkBox_6")
+        sizePolicy2.setHeightForWidth(self.checkBox_6.sizePolicy().hasHeightForWidth())
+        self.checkBox_6.setSizePolicy(sizePolicy2)
+        self.checkBox_6.setMaximumSize(QSize(16777215, 10))
+
+        self.horizontalLayout_7.addWidget(self.checkBox_6)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_7, 3, 0, 1, 1)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        sizePolicy1.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
+        self.label_4.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout_2.addWidget(self.label_4)
+
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+        sizePolicy1.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
+        self.label_5.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout_2.addWidget(self.label_5)
+
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+        sizePolicy.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout_2.addWidget(self.label_6)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_2, 2, 0, 1, 1)
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
@@ -133,21 +232,12 @@ class Ui_MainWindow(object):
         self.horizontalLayout_4.addWidget(self.next_button)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_4, 3, 0, 1, 1)
-
-        self.treeWidget = QTreeWidget(self.centralwidget)
-        __qtreewidgetitem = QTreeWidgetItem(self.treeWidget)
-        __qtreewidgetitem.setFlags(Qt.ItemIsSelectable|Qt.ItemIsUserCheckable|Qt.ItemIsEnabled);
-        __qtreewidgetitem.setCheckState(0, Qt.Unchecked);
-        self.treeWidget.setObjectName(u"treeWidget")
-        self.treeWidget.setMaximumSize(QSize(16777215, 100))
-
-        self.gridLayout.addWidget(self.treeWidget, 4, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout_4, 8, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menuBar = QMenuBar(MainWindow)
         self.menuBar.setObjectName(u"menuBar")
-        self.menuBar.setGeometry(QRect(0, 0, 407, 24))
+        self.menuBar.setGeometry(QRect(0, 0, 476, 24))
         self.menu = QMenu(self.menuBar)
         self.menu.setObjectName(u"menu")
         self.menu.setGeometry(QRect(446, 259, 99, 110))
@@ -175,14 +265,12 @@ class Ui_MainWindow(object):
         self.label_1.setText("")
         self.label_2.setText("")
         self.label_3.setText("")
-        self.label_4.setText("")
-        self.label_5.setText("")
-        self.label_6.setText("")
-        self.label_7.setText("")
-        self.label_8.setText("")
-        self.label_9.setText("")
-        self.prev_button.setText(QCoreApplication.translate("MainWindow", u"\u4e0a\u4e00\u9875", None))
-        self.next_button.setText(QCoreApplication.translate("MainWindow", u"\u4e0b\u4e00\u9875", None))
+        self.checkBox_1.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_2.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_3.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_7.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_8.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_9.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
         ___qtreewidgetitem = self.treeWidget.headerItem()
         ___qtreewidgetitem.setText(0, QCoreApplication.translate("MainWindow", u"1", None));
 
@@ -192,6 +280,17 @@ class Ui_MainWindow(object):
         ___qtreewidgetitem1.setText(0, QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u9879\u76ee", None));
         self.treeWidget.setSortingEnabled(__sortingEnabled)
 
+        self.label_7.setText("")
+        self.label_8.setText("")
+        self.label_9.setText("")
+        self.checkBox_4.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_5.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.checkBox_6.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.label_4.setText("")
+        self.label_5.setText("")
+        self.label_6.setText("")
+        self.prev_button.setText(QCoreApplication.translate("MainWindow", u"\u4e0a\u4e00\u9875", None))
+        self.next_button.setText(QCoreApplication.translate("MainWindow", u"\u4e0b\u4e00\u9875", None))
 #if QT_CONFIG(accessibility)
         self.menuBar.setAccessibleName("")
 #endif // QT_CONFIG(accessibility)
--- con.py
+++ con.py
@@ -12,6 +12,8 @@ import shutil
 class MainWindow(QMainWindow):
     @Slot()
     def load_image(self):
+        if not self.ui.prev_button.isEnabled():
+            self.ui.prev_button.setEnabled(True)
         self.up = self.count + 9
         data = interface(self.up)
         if not os.path.exists("temp"):
@@ -24,21 +26,46 @@ class MainWindow(QMainWindow):
         for member in data:
             for thumbnail in member["images"]:
                 current_thumbnail.append(thumbnail)
-        print(current_thumbnail)
-        print(self.count)
+        # print(current_thumbnail)
+        # print(self.count)
         current_thumbnail = current_thumbnail[self.count:]
         # print(self.count, self.up, current_thumbnail)
         for item in current_thumbnail:
             index += 1
             file_name = os.path.join(os.path.abspath("temp"), str(self.count + 1) + ".webp")
+            if not os.path.exists(file_name):
+                urlretrieve(item, file_name)
             # print(file_name)
-            urlretrieve(item, file_name)
             label_controller = "self.ui.label_" + str(index) + '.setPixmap(QPixmap(file_name))'
+            status = "self.ui.label_" + str(index) + ".setEnabled(False)"
+            checkbox_controller = "self.ui.checkBox_" + str(index) + ".setChecked(False)"
             # order = "self.ui.label_" + str(i) + ".setAlignment(Qt.AlignCenter)"
             exec(label_controller)
+            exec(status)
+            exec(checkbox_controller)
             self.count += 1
             # exec(order)
         self.show()
+
+    @Slot()
+    def prev_(self):
+        self.count -= 9
+        if self.count - 9 == 0:
+            self.ui.prev_button.setEnabled(False)
+        current_thumbnail = []
+        index = 0
+        # print(self.count)
+        for item in range(self.count - 8, self.count):
+            index += 1
+            # print(item)
+            file_name = os.path.join(os.path.abspath("temp"), str(item) + ".webp")
+            label_controller = "self.ui.label_" + str(index) + '.setPixmap(QPixmap(file_name))'
+            status = "self.ui.label_" + str(index) + ".setEnabled(False)"
+            checkbox_controller = "self.ui.checkBox_" + str(index) + ".setChecked(False)"
+            # print(label_controller)
+            exec(label_controller)
+            exec(status)
+            exec(checkbox_controller)
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -48,6 +75,19 @@ class MainWindow(QMainWindow):
         self.count = 0
         self.load_image()
         self.ui.next_button.clicked.connect(self.load_image)
+        self.ui.prev_button.clicked.connect(self.prev_)
+        # self.ui.checkBox_1
+        for value in range(1, 10):
+            command = "self.ui.checkBox_" + str(value) + ".stateChanged.connect(self.checkbox)"
+            exec(command)
+        self.ui.prev_button.setEnabled(False)
+
+    @Slot()
+    def checkbox(self):
+        for value in range(1, 10):
+            photo_status = ("self.ui.label_" + str(value) + ".setEnabled(True) if self.ui.checkBox_" +
+                            str(value) + ".isChecked() else self.ui.label_" + str(value) + ".setEnabled(False)")
+            exec(photo_status)
 
 
 if __name__ == "__main__":
